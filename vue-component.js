c5d85b0430dc9d246403f644b883cbd6b523732c
SK엠앤서비스 홍순민 <carrot@sk.com>
2020-05-07 16:22:40 +0900

    CSS 적용

--- vue-component.js
+++ vue-component.js
@@ -1,9 +1,9 @@
 Vue.component('countable-input', {
   inheritAttrs: false,
-  props: ['label', 'value'],
+  props: ['value', 'max-length', 'base-class', 'count-class', 'clear-class'],
   computed: {
   	count: function() {
-  		return this.value.length;
+  		return this.byteLength(this.value);
 	},
     inputListeners: function () {
       var vm = this
@@ -16,22 +16,77 @@ Vue.component('countable-input', {
         {
           // This ensures that the component works with v-model
           input: function (event) {
-            vm.$emit('input', event.target.value);
+          	var string = event.target.value
+          	stringByteLength = (function(s,b,i,c){
+    			for(b=i=0;c=s.charCodeAt(i++);){
+    				var t = c>>11?3:c>>7?2:1;
+    				b+=t;
+    				if (b > vm.maxLength) {
+    					b-=t;
+    					string = s.substring(0, i - 1);
+    					vm.$refs.input.value = string;
+    					break;
+					}
+    			}
+    			return b
+			})(string);
+            vm.$emit('input', string);
           }
         }
       )
     }
   },
+  methods: {
+  	clear: function() {
+  		this.$emit('input', '');
+	},
+	byteLength: function(s) {
+	    var l= 0;
+		for(var idx=0; idx < s.length; idx++) {
+			var c = escape(s.charAt(idx));
+			if( c.length==1 ) l ++;
+			else if( c.indexOf("%u")!=-1 ) l += 2;
+			else if( c.indexOf("%")!=-1 ) l += c.length/3;
+		}
+		return l;
+	}
+  },
   template: `
-  	<div>
-  	{{ label }}
+  	<div :class="baseClass">
   		<input
   			v-bind="$attrs"
   			v-bind:value="value"
   			v-on="inputListeners"
+  			ref="input"
   		>
-  		<span>{{ count }}</span>
-  		<button>delete</button>
+  		<span :class="countClass">{{ count }}/{{ maxLength }}</span>
+  		<button :class="clearClass" @click="clear">X</button>
   	</div>
 		  `
 });
+
+/*
+.input-txt {
+				position: relative;
+				border: solid;
+			}
+			.input-txt input {
+				position: relative;
+				width: 100%;
+				padding-right: 60px;
+			}
+			.input-txt .count {
+				position: absolute;
+				top: 0; right: 0;
+				width: 40px; height: 100%;
+				text-align: right;
+				padding-right: 5px;
+			}
+			.input-txt .clear {
+				position: absolute;
+				top: 0; right: 40px;
+				width: 20px; height: 100%;
+				border: none;
+			}
+<countable-input base-class="input-txt" clear-class="clear" count-class="count" v-model="text" max-length="10"></countable-input>
+ */
