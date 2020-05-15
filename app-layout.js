8fe467f3e83ab433c470201ffbc6f52cee7f3fc9
SK엠앤서비스 홍순민 <carrot@sk.com>
2020-05-15 16:28:59 +0900

    코드 포멧팅, countable-input에 focus/blur 상태변화에 따른 데이터 삭제 버튼 노출 기능 추가

--- app-layout.js
+++ app-layout.js
@@ -1,15 +1,15 @@
 Vue.component('app-layout', {
   template: `
-	<div class="container">
-	  <header>
-	    <slot name="header"></slot>
-	  </header>
-	  <main>
-	    <slot></slot>
-	  </main>
-	  <footer>
-	    <slot name="footer"></slot>
-	  </footer>
-	</div>
-		  `
+    <div class="container">
+      <header>
+        <slot name="header"></slot>
+      </header>
+      <main>
+        <slot></slot>
+      </main>
+      <footer>
+        <slot name="footer"></slot>
+      </footer>
+    </div>
+    `
 });
--- vue-component.js
+++ vue-component.js
@@ -1,10 +1,15 @@
 Vue.component('countable-input', {
   inheritAttrs: false,
   props: ['value', 'max-length', 'base-class', 'count-class', 'clear-class'],
+  data: function () {
+    return {
+      hasFocus: false
+    }
+  },
   computed: {
-  	count: function() {
-  		return this.byteLength(this.value);
-	},
+    count: function() {
+      return this.byteLength(this.value);
+  },
     inputListeners: function () {
       var vm = this
       // `Object.assign` merges objects together to form a new object
@@ -16,20 +21,20 @@ Vue.component('countable-input', {
         {
           // This ensures that the component works with v-model
           input: function (event) {
-          	var string = event.target.value
-          	stringByteLength = (function(s,b,i,c){
-    			for(b=i=0;c=s.charCodeAt(i++);){
-    				var t = c>>11?3:c>>7?2:1;
-    				b+=t;
-    				if (b > vm.maxLength) {
-    					b-=t;
-    					string = s.substring(0, i - 1);
-    					vm.$refs.input.value = string;
-    					break;
-					}
-    			}
-    			return b
-			})(string);
+            var string = event.target.value;
+            (function(s,b,i,c) {
+              for(b=i=0;c=s.charCodeAt(i);i++) {
+                var t = c>>11?3:c>>7?2:1;
+                b+=t;
+                if (b > vm.maxLength) {
+                  b-=t;
+                  string = s.substring(0, i);
+                  vm.$refs.input.value = string;
+                  break;
+                }
+              }
+              return b;
+            })(string);
             vm.$emit('input', string);
           }
         }
@@ -37,56 +42,56 @@ Vue.component('countable-input', {
     }
   },
   methods: {
-  	clear: function() {
-  		this.$emit('input', '');
-	},
-	byteLength: function(s) {
-	    var l= 0;
-		for(var idx=0; idx < s.length; idx++) {
-			var c = escape(s.charAt(idx));
-			if( c.length==1 ) l ++;
-			else if( c.indexOf("%u")!=-1 ) l += 2;
-			else if( c.indexOf("%")!=-1 ) l += c.length/3;
-		}
-		return l;
-	}
+    clear: function() {
+      console.log('clear button clicked.');
+      this.$emit('input', '');
+    },
+    byteLength: function(s) {
+      byteLength = (function(s,b,i,c) {
+        for(b=i=0;c=s.charCodeAt(i++);b+=c>>11?3:c>>7?2:1);
+        return b;
+      })(s);
+      return byteLength;
+    }
   },
   template: `
-  	<div :class="baseClass">
-  		<input
-  			v-bind="$attrs"
-  			v-bind:value="value"
-  			v-on="inputListeners"
-  			ref="input"
-  		>
-  		<span :class="countClass">{{ count }}/{{ maxLength }}</span>
-  		<button :class="clearClass" @click="clear">X</button>
-  	</div>
-		  `
+    <div :class="baseClass">
+      <input
+        v-bind="$attrs"
+        v-bind:value="value"
+        v-on="inputListeners"
+        v-on:focus="hasFocus = true"
+        v-on:blur="hasFocus = false"
+        ref="input"
+      >
+      <span :class="countClass">{{ count }}/{{ maxLength }}</span>
+      <button :class="clearClass" @mousedown="clear" v-show="hasFocus && value.length > 0">X</button>
+    </div>
+    `
 });
 
 /*
 .input-txt {
-				position: relative;
-				border: solid;
-			}
-			.input-txt input {
-				position: relative;
-				width: 100%;
-				padding-right: 60px;
-			}
-			.input-txt .count {
-				position: absolute;
-				top: 0; right: 0;
-				width: 40px; height: 100%;
-				text-align: right;
-				padding-right: 5px;
-			}
-			.input-txt .clear {
-				position: absolute;
-				top: 0; right: 40px;
-				width: 20px; height: 100%;
-				border: none;
-			}
+  position: relative;
+  border: solid;
+}
+.input-txt input {
+  position: relative;
+  width: 100%;
+  padding-right: 60px;
+}
+.input-txt .count {
+  position: absolute;
+  top: 0; right: 0;
+  width: 40px; height: 100%;
+  text-align: right;
+  padding-right: 5px;
+}
+.input-txt .clear {
+  position: absolute;
+  top: 0; right: 40px;
+  width: 20px; height: 100%;
+  border: none;
+}
 <countable-input base-class="input-txt" clear-class="clear" count-class="count" v-model="text" max-length="10"></countable-input>
  */
